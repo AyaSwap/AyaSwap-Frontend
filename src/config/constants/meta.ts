c0846b712e9c64fc1a9080b1ffe20b52d9e64e6a
AyaSwap <86141106+AyaSwap@users.noreply.github.com>
2021-06-19 14:36:42 +0700

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,7 +1,7 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'BecoSwap',
+  title: 'DekoFinance',
   description:
     'The most popular AMM on BSC by user count! Earn DEKO through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by DekoFinance), NFTs, and more, on a platform you can trust.',
   image: 'https://becoswap.com/images/hero.jpg',
