eb9bb37f4d6b972ca5da527bfe12776a13c6d4ab
AyaSwap <86141106+AyaSwap@users.noreply.github.com>
2021-06-28 07:59:17 +0700

    USDT-BNB

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -43,6 +43,16 @@ const farms: FarmConfig[] = [
     quoteToken: tokens.wbnb,
   },
   {
+    pid: 14,
+    lpSymbol: 'USDT-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x16b9a82891338f9ba80e2d6970fdda79d1eb0dae',
+    },
+    token: tokens.usdt,
+    quoteToken: tokens.wbnb,
+  },
+  {
     pid: 4,
     lpSymbol: 'ETH-BNB LP',
     lpAddresses: {
